5224e3bd3b3b2718d6dcb87fbc2cc910a8f2f30d
unknown <neosou@gmail.com>
2022-03-08 22:48:59 +0900

    bank1:

--- main.go
+++ main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/goFirst/something"
+	"std/github.com/bank/accounts"
 )
 
 func main() {
-	fmt.Println("start")
-	something.SayBye()
+	account := accounts.NewAccouont("lsh")
+	account.Deposit(10)
+	fmt.Println(account.Balance())
+	err := account.Withdraw(20)
+	if err != nil {
+		fmt.Println(err)
+	}
 
+	fmt.Println(account.Balance())
 }
